915b2db9be6aef9722e047b7d8a5297fc973611b
Rawahul Islam <125194423+rawahulislam@users.noreply.github.com>
2023-06-14 23:11:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+document.title = "Qtify"
 import App from "./App";
 
 const rootElement = document.getElementById("root");
